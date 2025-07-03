ea64f6317239a91aab25d69dd44357161db663f1
Lin77-da <soulingkeo@gmail.com>
2025-07-03 09:38:17 +0200

    Update vs_code.py

--- vs_code.py
+++ vs_code.py
@@ -11,7 +11,7 @@ with st.sidebar:
 @st.cache_data
 def load_data_from_gdrive(file_id):
     url = f"https://drive.google.com/uc?id=1b_yXHGs5Ms4O3wmUrxTLAXiJPHLm2ZXS"
-    return pd.read_csv(url,sep=";")
+    return pd.read_csv(url,sep=",")
 
 file_id = "1b_yXHGs5Ms4O3wmUrxTLAXiJPHLm2ZXS"
 
